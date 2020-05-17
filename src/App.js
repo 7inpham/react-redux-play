a636a4d3ad807bdca44e3214968836440984e62e
Tin Pham <7inpham@gmail.com>
2020-05-17 22:18:01 +0700

    try redux thunk

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
 import './App.css';
 import Hello from './components/Hello';
 
 function App({name, dispatch}) {
 
-  const handleChangeName = (e) => {
-    dispatch({
+  const createSetNameAction = (value) => {
+    return {
       type: 'SET_NAME',
-      value: e.target.value
-    })
+      value
+    }
+  }
+
+  const createSetNameActionAsync = (value) => {
+    return (dispatch) => {
+      setTimeout(() => {
+        dispatch({
+          type: 'SET_NAME',
+          value
+        })
+      }, 1000);
+    };
+  }
+
+  const handleChangeName = (e) => {
+    const value = e.target.value
+    if (value.includes('async')) {
+      dispatch(createSetNameActionAsync(value))
+    } else {
+      dispatch(createSetNameAction(value))
+    }
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -20,7 +21,7 @@ const reducer = (state = initialState, action) => {
   return state
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
